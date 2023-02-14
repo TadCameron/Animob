2f655768bdcf1c74634510141848d62342c520cc
jaiden faulkner <jaidenfaulkner@gmail.com>
2023-02-14 15:57:23 -0600

    messed with some more authentication stuff

--- accounts/queries/accounts.py
+++ accounts/queries/accounts.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 from pymongo.errors import DuplicateKeyError
+from typing import Optional
+# from client import Queries
 
 class DuplicateAccountError(ValueError):
     pass
@@ -16,10 +18,39 @@ class AccountOut(BaseModel):
 class AccountOutWithPassword(AccountOut):
     hashed_password: str
 
-class AccountQueries(Queries):
-    #region properties
 
-    def get(self, username:str) -> AccountOutWithPassword:
+class AccountQueries:
+    def __init__(self, collection):
+        self.collection = collection
+
+    def get(self, username: str) -> Optional[AccountOutWithPassword]: #we might not need this optional thing
+        account_data = self.collection.find_one({'username': username})
+        if account_data is None:
+            return None
+        return AccountOutWithPassword(
+            id=str(account_data['_id']),
+            username=account_data['username'],
+            hashed_password=account_data['hashed_password']
+        )
+
+    def create(self, account: AccountIn, hashed_password: str) -> AccountOutWithPassword:
+        account_data = account.dict()
+        account_data['hashed_password'] = hashed_password
+        try:
+            result = self.collection.insert_one(account_data)
+        except DuplicateKeyError:
+            raise DuplicateAccountError(f"Account with username {account.username} already exists")
+        account_out = AccountOutWithPassword(
+            id=str(result.inserted_id),
+            username=account.username,
+            hashed_password=hashed_password
+        )
+        return account_out
+
+# class AccountQueries(Queries):
+#     #region properties
+
+#     def get(self, username:str) -> AccountOutWithPassword:
 
 
-    def create(self, user: AccountIn, hashed_password: str) -> AccountOutWithPassword:
+#     def create(self, user: AccountIn, hashed_password: str) -> AccountOutWithPassword:
