755bbca67f1552087f8f904ecf8fd2a27c6d257a
Sarah Floyd <sarahm_floyd@yahoo.com>
2023-02-15 13:37:09 -0500

    updating queries

--- animob/queries/accounts.py
+++ animob/queries/accounts.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from pymongo.errors import DuplicateKeyError
 from .client import Queries
+from pymongo import MongoClient
 
 class DuplicateAccountError(ValueError):
     pass
@@ -38,3 +39,8 @@ class AccountQueries(Queries):
             raise DuplicateAccountError()
         props["id"] = str(props["_id"])
         return AccountOut(**props)
+
+    def delete_account(self, id: str) -> int:
+        with MongoClient(host=self.host, port=self.port) as client:
+            db = client[self.db_name]
+            db.users.delete_one({"id": id})
