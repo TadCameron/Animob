2bcfd8175f9c064ba073cd101bf01e4d9d4e4004
Sarah Floyd <sarahm_floyd@yahoo.com>
2023-02-15 17:06:40 -0500

    anime routers and queries

---
+++ animob/routers/anime.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from queries.anime import AnimeQueries
+
+router = APIRouter()
+
+@router.get("/")
+def get_popular_anime():
+
+
+@router.get("/")
+def get_anime_detail():
+
+
+@router.get("/")
+def get_top_airing_anime():
+
+
+@router.get("/")
+def get_genres():
+
+
+@router.get("/")
+def get_anime_by_genre():
