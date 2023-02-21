0f506e8285afde6f362894eb944b6b804a6395ba
Sarah Floyd <sarahm_floyd@yahoo.com>
2023-02-21 18:06:02 -0500

    updated store

--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
+import { authApiSlice } from './authApi'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [authApiSlice.reducerPath]: authApiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApiSlice.middleware),
 })
 
+setupListeners(store.dispatch)
 // apislice w endpoints
 // mutations
