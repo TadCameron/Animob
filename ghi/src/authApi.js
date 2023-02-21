6b627ca2a5f284c8340d297dce73e6b2ff4a70a3
Sarah Floyd <sarahm_floyd@yahoo.com>
2023-02-21 17:27:44 -0500

    fixed authapi

--- ghi/src/authApi.js
+++ ghi/src/authApi.js
@@ -1,43 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const authApi = createApi({
-  reducerPath: 'authentication',
-  tagTypes: ['Token'],
+export const authApiSlice = createApi({
+  reducerPath: 'auth',
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.REACT_APP_SAMPLE_SERVICE_API_HOST,
   }),
-  endpoints: builder => ({
-    login: builder.mutation({
-      query: info => {
-        let formData = null;
-        if (info instanceof HTMLElement) {
-          formData = new FormData(info);
-        } else {
-          formData = new FormData();
-          formData.append('username', info.email);
-          formData.append('password', info.password);
-        }
-        return {
-          url: '/token',
-
-          method: 'post',
-          body: formData,
-          credentials: 'include',
-        };
-      },
-      invalidatesTags: result => {
-        return (result && ['Account']) || [];
-      },
-    }),
-
-    logOut: builder.mutation({
-      query: () => ({
-        url: '/token',
-        method: 'delete',
-        credentials: 'include',
-      }),
-      invalidatesTags: ['Account', 'Token'],
-    }),
   tagTypes: ['Account', 'Books', 'Token'],
   endpoints: builder => ({
     signUp: builder.mutation({
@@ -51,17 +18,51 @@ export const authApi = createApi({
       invalidatesTags: result => {
         return (result && ['Token']) || [];
       },
-    }),
 
+    }),
+    logIn: builder.mutation({
+      query: info => {
+        let formData = null;
+        if (info instanceof HTMLElement) {
+          formData = new FormData(info);
+        } else {
+          formData = new FormData();
+          formData.append('username', info.email);
+          formData.append('password', info.password);
+        }
+        return {
+          url: '/token',
+          method: 'post',
+          body: formData,
+          credentials: 'include',
+        };
+      },
+      providesTags: ['Account'],
+      invalidatesTags: result => {
+        return (result && ['Token']) || [];
+      },
+    }),
+    logOut: builder.mutation({
+      query: () => ({
+        url: '/token',
+        method: 'delete',
+        credentials: 'include',
+      }),
+      invalidatesTags: ['Account', 'Token'],
+    }),
     getToken: builder.query({
       query: () => ({
         url: '/token',
-
         credentials: 'include',
       }),
       providesTags: ['Token'],
     }),
   }),
-})
+});
 
-export const { useSignUpMutation, useLogOutMutation, useLoginMutation, useGetTokenQuery } = authApi;
+export const {
+  useGetTokenQuery,
+  useLogInMutation,
+  useLogOutMutation,
+  useSignUpMutation,
+} = authApiSlice;
