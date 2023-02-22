e6e759b1644c934d538bc6363322c73b1ae25993
Sarah Floyd <sarahm_floyd@yahoo.com>
2023-02-22 13:25:22 -0500

    deleted everything

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,34 +1,22 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-// import MainPage from './MainPage';
-// import Nav from './Nav';
-// import { BrowserRouter, Routes, Route } from 'react';
+import { AuthProvider, useToken } from './store/useTokenSlice'
+import { useNavigate } from 'react-router-dom';
 
+
+function GetToken() {
+    // Get token from JWT cookie (if already logged in)
+    useToken();
+    return null
+}
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log("fastapi url: ", url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, []);
-
-
+  return (
+    <AuthProvider>
+      <GetToken />
+      {/* All of your other components, here */}
+    </AuthProvider>
+  );
 }
 
 export default App;
