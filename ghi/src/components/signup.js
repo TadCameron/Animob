4437c4cf9487c46f819f21b583f0e6b278013f68
rowan <trowanjr@gmail.com>
2023-02-23 15:08:30 -0500

    idk

--- ghi/src/components/signup.js
+++ ghi/src/components/signup.js
@@ -1,18 +1,7 @@
 import { useToken } from './useToken'
 import { useState } from 'react';
 
-function BootstrapInput(props) {
-    const {id, placeholder, labelText, value, onChange, type } = props;
-
-    return (
-        <div className="mb-4">
-            <label htmlFor={id} className="form-label">{labelText}</label>
-            <input value={value} onChange={onChange} required type={type} className="form-control" id={id} placeholder={placeholder} ></input>
-        </div>
-    );
-}
-
-function SignupForm() {
+function Signup() {
 const { token, signup } = useToken();
 const [full_name, setFullName] = useState('');
 const [email, setEmail] = useState('');
@@ -24,49 +13,14 @@ async function handleSubmit(event) {
     signup(full_name, email, username, password)
     }
 
-    return (
-    <div className="accountpagecontainer">
-        <div className="card" >
-        <form onSubmit={handleSubmit}>
-<<<<<<< HEAD
-            <BootstrapInput
-=======
-            <h5 class="card-title">Sign up</h5>
-             <BootstrapInput
->>>>>>> ae711136ebb55e7471b4b663f727a4d65552599b
-                id="name"
-                placeholder="name"
-                // labelText="name"
-                value={full_name}
-                onChange={e => setFullName(e.target.value)}
-                type="text" />
-            <BootstrapInput
-                id="email"
-                placeholder="email"
-                // labelText="email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                type="email" />
-            <BootstrapInput
-                id="username"
-                placeholder="username"
-                // labelText="your username"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-                type="username" />
-            <BootstrapInput
-                id="password"
-                placeholder="password"
-                // labelText="Password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                type="password" />
+  return (
+    <form onSubmit={handleSubmit}>
+        <input type="full_name" placeholder="Full Name" value={full_name} onChange={(e) => setFullName(e.target.value)} />
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Sign up</button>
+    </form>
+);}
 
-            <button type="submit" className="btn btn-light">SUBMIT</button>
-        </form>
-        </div>
-    </div>
-    );
-    }
-
-    export default SignupForm
+export default Signup;
