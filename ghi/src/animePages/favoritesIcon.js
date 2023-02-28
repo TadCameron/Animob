90b3e53480f443fbb185a8f021b1c51e16b529e4
Tad Cameron <tad.w.cameron@gmail.com>
2023-02-28 15:40:00 -0500

    plusminus button working

--- ghi/src/animePages/favoritesIcon.js
+++ ghi/src/animePages/favoritesIcon.js
@@ -7,28 +7,24 @@ function FavoritesIcon(props) {
     const [ animeId, setAnimeId ] = useState(props.animeId);
     const [ animeTitle, setAnimeTitle ] = useState(props.animeTitle);
     const [ animeImg, setAnimeImg ] = useState(props.animeImg);
-    const { token } = useAuthContext();
     const [ found, setFound ] = useState(false);
-    console.log(props.favorites);
-    // console.log(props.animeId);
+    const { token } = useAuthContext();
 
     function PlusMinusButton() {
         for (const anime of props.favorites){
             // console.log(anime)
             // console.log(animeId)
-        if (anime['animeId'] === animeId){
-            setFound(true);
-        } if (found === false) {
+            if (anime['animeId'] === animeId){
+                setFound(true);
+                break
+            }}
+        if (found === false) {
             return (<p>+</p>)
-        }
-
-        }
-    }
-
-    }
+        } else {
+            return (<p>-</p>)
+        }}
 
     async function addToFavorites() {
-        console.log('hello')
         if (favorites === false) {
             const URL = (`${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/favorites`);
             const favResponse = await fetch(URL, {method: 'POST', headers: {Authorization: `Bearer ${token}`}, body: JSON.stringify({animeId, animeTitle, animeImg
@@ -38,27 +34,11 @@ function FavoritesIcon(props) {
             }
         } else {
             const URL = (`${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/favorites`);
-            const favResponse = await fetch(URL, {method: 'DELETE', headers: {Authorization: `Bearer ${token}`}, credentials: 'include'});
+            const favResponse = await fetch(URL, {method: 'DELETE', headers: {Authorization: `Bearer ${token}`}, body: JSON.stringify({animeId, animeTitle, animeImg
+            }), credentials: 'include'});
+
             if (favResponse.ok) {
-                const data = await favResponse.json();
-                for (let i = 0; i < data.length; i++) {
-                    if (data[i]["favorite_id"] === props.favorite_id) {
-                        const favorite_id = data[i]["favorite_id"];
-                        const url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/favorites/${favorite_id}`
-                        const response = await fetch(url, {
-                            method: "DELETE", headers: {
-                                'accept': 'application/json'
-                            }
-                        });
-                        if (response.ok) {
-                            setFavorites(false);
-
-            // if (favResponse.ok) {
-            //     setFavorites(false);
-                        };
-                    };
-                };
-
+                setFavorites(false);
             }
         }
 
