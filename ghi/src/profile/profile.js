8e3ac2c10e8ec7fd09f54bb76fa1d0a32e4c506a
Sarah Floyd <sarahm_floyd@yahoo.com>
2023-03-01 13:13:39 -0500

    profile page changes

--- ghi/src/profile/profile.js
+++ ghi/src/profile/profile.js
@@ -25,28 +25,57 @@ function Profile() {
 
 
 return (
-    <>
-    <div className="genrecontainer">
-        <div className="row row-cols-2 row-cols-md-3 g-4">
-            {favorites.map(anime => {
-            return (
-                <div key={anime.animeId}>
-                    <div className="col d-flex justify-content-center">
-                        <div className="card" id="animecard">
-                            <img src={anime.animeImg} className="card-img-top" alt="" id="animeimage"></img>
-                                <div className="card-body">
-                                <h5 className="card-title">{anime.animeTitle}</h5>
-                                <Link className="btn btn-primary" to={`/anime-detail/${anime.animeId}`}>See Details</Link>
-                                </div>
+  <>
+    <div className="mainpagecontainer">
+      <div className="row row-cols-2 row-cols-md-3 g-4">
+        <div class="container text-center my-3">
+          <h2 class="font-weight-light">Bootstrap Multi Slide Carousel</h2>
+          <div class="row mx-auto my-auto">
+            <div
+              id="recipeCarousel"
+              class="carousel slide"
+              data-bs-ride="carousel"
+            >
+              <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                  {/* <div class="col-md-3"> */}
+                    {favorites.map((anime) => {
+                      return (
+                        <div key={anime.animeId}>
+                          <div className="col d-flex justify-content-center">
+                            <div className="card" id="animecard">
+                              <img
+                                src={anime.animeImg}
+                                className="card-img-top"
+                                alt=""
+                                id="animeimage"
+                              ></img>
+                              <div className="card-body">
+                                <h5 className="card-title">
+                                  {anime.animeTitle}
+                                </h5>
+                                <Link
+                                  className="btn btn-primary"
+                                  to={`/anime-detail/${anime.animeId}`}
+                                >
+                                  See Details
+                                </Link>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                    </div>
+                      );
+                    })}
+                  {/* </div> */}
                 </div>
-            );
-            })}
+              </div>
             </div>
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </div>
+  </>
+);
 }
 
 export default Profile
