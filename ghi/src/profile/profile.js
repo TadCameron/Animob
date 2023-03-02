e9d34d3700be74b39acefaaf2e44c57c95c86c0f
Sarah Floyd <sarahm_floyd@yahoo.com>
2023-03-01 19:10:39 -0500

    we got a carousel

--- ghi/src/profile/profile.js
+++ ghi/src/profile/profile.js
@@ -1,84 +1,102 @@
-import { useEffect, useState, } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../components/useToken";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import FavoritesIcon from "../animePages/favoritesIcon";
 import DeleteIcon from "../animePages/deleteIcon";
 
-
 function Profile(props) {
-    const [favorites, setFavorites] = useState([]);
-    const { token } = useAuthContext();
+  const [favorites, setFavorites] = useState([]);
+  const { token } = useAuthContext();
 
-    const getData = async () => {
-        const response = await fetch(`${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/favorites/`, {
+  const getData = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/favorites/`,
+      {
         credentials: "include",
-      });
+      }
+    );
 
-        if (response.ok) {
-            const data = await response.json();
-            setFavorites(data.favorites);
-        }
+    if (response.ok) {
+      const data = await response.json();
+      setFavorites(data.favorites);
     }
+  };
 
-        useEffect(() => {
-        getData()
-    }, [])
+  useEffect(() => {
+    getData();
+  }, []);
 
-
-
-
-return (
-  <>
-    <div className="mainpagecontainer">
-      <div className="row row-cols-2 row-cols-md-3 g-4">
-        <div class="container text-center my-3">
-          <h2 class="font-weight-light">Bootstrap Multi Slide Carousel</h2>
-          <div class="row mx-auto my-auto">
-            <div
-              id="recipeCarousel"
-              class="carousel slide"
-              data-bs-ride="carousel"
-            >
-              <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active">
-                  {/* <div class="col-md-3"> */}
-                    {favorites.map((anime) => {
-                      return (
-                        <div key={anime.animeId}>
-                          <div className="col d-flex justify-content-center">
-                            <div className="card" id="animecard">
-                              <img
-                                src={anime.animeImg}
-                                className="card-img-top"
-                                alt=""
-                                id="animeimage"
-                              ></img>
-                              <div className="card-body">
-                                <h5 className="card-title">
-                                  {anime.animeTitle}
-                                </h5>
-                                <Link
-                                  className="btn btn-primary"
-                                  to={`/anime-detail/${anime.animeId}`}
-                                >
-                                  See Details
-                                </Link>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      );
-                    })}
-                  {/* </div> */}
-                </div>
-              </div>
+  let favCarousel = favorites.map(function (anime) {
+    return (
+      <div key={anime.animeId}>
+        <div className="col d-flex justify-content-center">
+          <div className="card" id="animecard">
+            <img
+              src={anime.animeImg}
+              className="card-img-top"
+              alt=""
+              id="animeimage"
+            ></img>
+            <div className="card-body">
+              <h5 className="card-title">{anime.animeTitle}</h5>
+              <Link
+                className="btn btn-primary"
+                to={`/anime-detail/${anime.animeId}`}
+              >
+                See Details
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    );
+  });
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+  return (
+    <>
+      <div className="mainpagecontainer">
+        <Carousel
+          swipeable={false}
+          draggable={false}
+          showDots={true}
+          responsive={responsive}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlay={false}
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          //   deviceType={this.props.deviceType}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
+          {favCarousel}
+        </Carousel>
+        ;
+      </div>
+    </>
+  );
 }
 
-export default Profile
+export default Profile;
