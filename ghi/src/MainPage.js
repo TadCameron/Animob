e7aac922dc9aec4b5beaad9b20817dbdc3055bc3
rowan <trowanjr@gmail.com>
2023-03-03 15:19:25 -0500

    profile page cards change

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,40 +1,64 @@
-import { NavLink } from "react-router-dom"
-import './index.css';
+import { NavLink } from "react-router-dom";
+import "./index.css";
 import { useAuthContext } from "./components/useToken";
 
-
 function MainPage() {
-  const { token } = useAuthContext()
+  const { token } = useAuthContext();
 
   return (
     <>
-
-    <div className="mainpagecontainer">
-      <div className="px-5 py-2 my-5 text-light position-absolute" id="main-view">
-        <h1 className="display-1 fw-bold text-center"></h1>
+      <div className="mainpagecontainer">
+        <div
+          className="px-5 py-2 my-5 text-light position-absolute"
+          id="main-view"
+        >
+          <h1 className="display-1 fw-bold text-center"></h1>
           <div className="mask rgba-black-light align-items-center">
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12 mb-4 white-text text-center">
-                    <h1 className="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown" data-wow-delay="0.3s"><strong>ANIMOB</strong></h1>
-                    {token ? (
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12 mb-4 white-text text-center">
+                  <h1
+                    className="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown"
+                    data-wow-delay="0.3s"
+                  >
+                    <strong>ANIMOB</strong>
+                  </h1>
+                  {token ? (
                     <div className="text-center p-2">
-                      <NavLink to="/profile" className="btn btn-outline-white text-white wow fadeInDown">Profile</NavLink>
-                      </div>
-                    ) : (
-                      <div className="text-center p-2">
-                        <NavLink to="/login" className="btn btn-outline-white text-white wow fadeInDown">Login</NavLink>
-                        <NavLink to="/signup" className="btn btn-outline-white text-white wow fadeInDown">Signup</NavLink>
+                      <NavLink
+                        to="/profile"
+                        className="btn btn-outline-white text-white wow fadeInDown"
+                      >
+                        Profile
+                      </NavLink>
                     </div>
-                    )}
-                    <hr class="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s"></hr>
-                  </div>
+                  ) : (
+                    <div className="text-center p-2">
+                      <NavLink
+                        to="/login"
+                        className="btn btn-outline-white text-white wow fadeInDown"
+                      >
+                        Login
+                      </NavLink>
+                      <NavLink
+                        to="/signup"
+                        className="btn btn-outline-white text-white wow fadeInDown"
+                      >
+                        Signup
+                      </NavLink>
+                    </div>
+                  )}
+                  <hr
+                    class="hr-light my-4 wow fadeInDown"
+                    data-wow-delay="0.4s"
+                  ></hr>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </>
+      </div>
+    </>
   );
 }
 
