5dedc70bb2b6b2120e7a2a85e42fd84f3524bf60
Sarah Floyd <sarahm_floyd@yahoo.com>
2023-03-03 20:17:37 -0500

    login page

--- ghi/src/components/login.js
+++ ghi/src/components/login.js
@@ -1,41 +1,231 @@
-import { useToken } from './useToken'
-import { useState } from 'react';
-import './accounts.css';
+import { useToken } from "./useToken";
+import { useState, useEffect } from "react";
+import "./accounts.css";
+import anime from "animejs";
 
-function BootstrapInput(props) {
-    const {id, placeholder, labelText, value, onChange, type } = props;
 
-    return (
-        <div className="mb-4">
-            <label htmlFor={id} className="form-label">{labelText}</label>
-            <input value={value} onChange={onChange} required type={type} className="form-control" id={id} placeholder={placeholder} ></input>
-        </div>
-    );
-}
+// function BootstrapInput(props) {
+//   const { id, placeholder, labelText, value, onChange, type } = props;
+
+//   return (
+//     <div classNameName="mb-4">
+//       <label htmlFor={id} classNameName="form-label">
+//         {labelText}
+//       </label>
+//       <input
+//         value={value}
+//         onChange={onChange}
+//         required
+//         type={type}
+//         classNameName="form-control"
+//         id={id}
+//         placeholder={placeholder}
+//       ></input>
+//     </div>
+//   );
+// }
 function Login() {
-const { token, login } = useToken();
-const [username, setUsername] = useState('');
-const [password, setPassword] = useState('');
+  const { token, login } = useToken();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-async function handleSubmit(event) {
-  event.preventDefault();
-    login(username, password)
-    localStorage.setItem(token, 'tokenValue')
-    }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    login(username, password);
+
+  }
+
+  // useEffect(() => {
+  //   var current = null;
+  //   document.querySelector("#username").addEventListener("focus", function (e) {
+  //     if (current) {current.pause()};
+  //     current = anime({
+  //       targets: "path",
+  //       strokeDashoffset: {
+  //         value: 0,
+  //         duration: 700,
+  //         easing: "easeOutQuart",
+  //       },
+  //       strokeDasharray: {
+  //         value: "240 1386",
+  //         duration: 700,
+  //         easing: "easeOutQuart",
+  //       },
+  //     });
+  //   });
+  //   document.querySelector("#password").addEventListener("focus", function (e) {
+  //     if (current) current.pause();
+  //     current = anime({
+  //       targets: "path",
+  //       strokeDashoffset: {
+  //         value: -336,
+  //         duration: 700,
+  //         easing: "easeOutQuart",
+  //       },
+  //       strokeDasharray: {
+  //         value: "240 1386",
+  //         duration: 700,
+  //         easing: "easeOutQuart",
+  //       },
+  //     });
+  //   });
+  //   document.querySelector("#submit").addEventListener("focus", function (e) {
+  //     if (current) current.pause();
+  //     current = anime({
+  //       targets: "path",
+  //       strokeDashoffset: {
+  //         value: -730,
+  //         duration: 700,
+  //         easing: "easeOutQuart",
+  //       },
+  //       strokeDasharray: {
+  //         value: "530 1386",
+  //         duration: 700,
+  //         easing: "easeOutQuart",
+  //       },
+  //     });
+  //   });
+  // });
+    useEffect(() => {
+    var current = null;
+    const username = document.querySelector("#username");
+    const password = document.querySelector("#password");
+    const submit = document.querySelector("#submit");
+
+    const handleUsernameFocus = () => {
+      if (current) {
+        current.pause();
+      }
+      current = anime({
+        targets: "path",
+        strokeDashoffset: {
+          value: 0,
+          duration: 700,
+          easing: "easeOutQuart",
+        },
+        strokeDasharray: {
+          value: "240 1386",
+          duration: 700,
+          easing: "easeOutQuart",
+        },
+      });
+    };
+
+    const handlePasswordFocus = () => {
+      if (current) {
+        current.pause();
+      }
+      current = anime({
+        targets: "path",
+        strokeDashoffset: {
+          value: -336,
+          duration: 700,
+          easing: "easeOutQuart",
+        },
+        strokeDasharray: {
+          value: "240 1386",
+          duration: 700,
+          easing: "easeOutQuart",
+        },
+      });
+    };
+
+    const handleSubmitFocus = () => {
+      if (current) {
+        current.pause();
+      }
+      current = anime({
+        targets: "path",
+        strokeDashoffset: {
+          value: -730,
+          duration: 700,
+          easing: "easeOutQuart",
+        },
+        strokeDasharray: {
+          value: "530 1386",
+          duration: 700,
+          easing: "easeOutQuart",
+        },
+      });
+    };
+
+    username.addEventListener("focus", handleUsernameFocus);
+    password.addEventListener("focus", handlePasswordFocus);
+    submit.addEventListener("focus", handleSubmitFocus);
+
+    return () => {
+      username.removeEventListener("focus", handleUsernameFocus);
+      password.removeEventListener("focus", handlePasswordFocus);
+      submit.removeEventListener("focus", handleSubmitFocus);
+    };
+  }, []);
+
+
 
   return (
-    <div className="accountpagecontainer">
-    <div className="card" >
-    <form onSubmit={handleSubmit}>
-      <h5 class="card-title">Log in</h5>
-       <BootstrapInput type="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-       <BootstrapInput type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-       <button type="submit" className="btn btn-light">LOG IN</button>
-    </form>
-    </div>
-    </div>
-);
-}
+    <div className="page">
+      <div className="container">
+        <div className="left">
+          <div className="login">Login</div>
+          <div className="eula">
+            By logging in you agree to the ridiculously long terms that you
+            didn't bother to read
+          </div>
+        </div>
+        <div className="right">
+          <svg viewBox="0 0 320 300">
+            <defs>
+              <linearGradient
+                inkscapecollect="always"
+                id="linearGradient"
+                x1="13"
+                y1="193.49992"
+                x2="307"
+                y2="193.49992"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop
+                  style={{ stopColor: "#ff00ff" }}
+                  offset="0"
+                  id="stop876"
+                />
+                <stop
+                  style={{ stopColor: "#ff0000" }}
+                  offset="1"
+                  id="stop878"
+                />
+              </linearGradient>
+            </defs>
+            <path d="m 40,120.00016 239.99984,-3.2e-4 c 0,0 24.99263,0.79932 25.00016,35.00016 0.008,34.20084 -25.00016,35 -25.00016,35 h -239.99984 c 0,-0.0205 -25,4.01348 -25,38.5 0,34.48652 25,38.5 25,38.5 h 215 c 0,0 20,-0.99604 20,-25 0,-24.00396 -20,-25 -20,-25 h -190 c 0,0 -20,1.71033 -20,25 0,24.00396 20,25 20,25 h 168.57143" />
+          </svg>
 
+            <form className="form" onSubmit={handleSubmit}>
+             
+              <label htmlFor="username">Username</label>
+              <input
+                type="username"
+                id="username"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button id="submit" type="submit" className="btn btn-light">
+                LOG IN
+              </button>
+            </form>
+
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
