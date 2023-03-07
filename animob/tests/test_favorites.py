3c7d7b1dc4b73a18ac4658e75bce6cf704fd219c
Sarah Floyd <sarahm_floyd@yahoo.com>
2023-03-07 13:07:53 -0500

    pytest 1

--- animob/tests/test_favorites.py
+++ animob/tests/test_favorites.py
@@ -1,7 +1,7 @@
 import json
 from fastapi.testclient import TestClient
+from routers.auth import authenticator
 from main import app
-from routers.auth import MyAuthenticator
 from queries.favorites import FavoritesIn, FavoritesOut, FavoritesQueries
 
 
@@ -13,47 +13,48 @@ def mock_get_current_account_data():
 client = TestClient(app=app)
 
 class MockFavoritesQuery:
-    def create(self, favorites:FavoritesIn, account_id: int) -> FavoritesOut:
-        favorites = []
-        return FavoritesOut(id=2, **favorites)
+    def create_favorite(self, favorite_in:FavoritesIn, account_id: str) -> FavoritesOut:
+
+        return FavoritesOut(id=2, account_id=account_id, **favorite_in.dict())
 
     def get_all_favorites(self, account_id):
         return [{"animeTitle": "Bleach",
         "animeId": "Bleach",
         "animeImg": "https://gogocdn.net/images/anime/B/bleach.jpg",
-        "account_id": 7},
+        "account_id": '7'},
 
         {"animeTitle": "Naruto",
         "animeId": "Naruto",
         "animeImg": "https://gogocdn.net/images/anime/N/naruto.jpg",
-        "account_id": 7}]
+        "account_id": '7'}]
 
     def get_favorite(self, favorite_id, account_id):
         return {"animeTitle": "Bleach",
         "animeId": "Bleach",
         "animeImg": "https://gogocdn.net/images/anime/B/bleach.jpg",
-        "account_id": 7}
+        "account_id": '7'}
 
     def delete_favorite(self, favorites:FavoritesIn, account_id: int) -> FavoritesOut:
         return True
 
-def test_mock_create_favorite ():
+def test_mock_create_favorite():
     #Arrange
+    print('hello')
     app.dependency_overrides[FavoritesQueries] = MockFavoritesQuery
     app.dependency_overrides[
-        MyAuthenticator.get_current_account_data
+        authenticator.get_current_account_data
     ] = mock_get_current_account_data
     expected = {"animeTitle": "Bleach",
         "animeId": "Bleach",
         "animeImg": "https://gogocdn.net/images/anime/B/bleach.jpg",
-        "account_id": 7}
+        "account_id": '7'}
 
     #Act
-    response = client.post("/favorites", json.dumps(expected))
+    response = client.post("/api/favorites", json.dumps(expected))
 
     #Assert (needs boolean)
     assert response.status_code == 200
-    assert response.json()["id"] == 2
+    assert response.json()["id"] == '2'
     assert response.json()["animeTitle"] == "Bleach"
     assert response.json()["animeId"] == "Bleach"
     assert response.json()["animeImg"] == "https://gogocdn.net/images/anime/B/bleach.jpg"
